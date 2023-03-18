faf6ef5dffff3eca2e5626e0719a4c251ecef9db
Fred Denis <fredjamo68@gmail.com>
2023-03-18 11:38:12 +0300

    Introdution to classes

--- funct.py
+++ funct.py
@@ -2,18 +2,21 @@
 
 def sum(numbers):
     total = 0
-    
+
     for num in numbers:
         # total = total + num
         total += num
     return total
 
+
 print("The sum: ", sum([8, 2, 3, 0, 7]))
 
-#Findind the multiples
+# Findind the multiples
+
+
 def multiples(numbers):
     total = 1
-    
+
     for numb in numbers:
         total = total * numb
 #     return total
@@ -22,18 +25,31 @@ def multiples(numbers):
 
 # Write a Python function to find the maximum of three numbers
 # 1, 5, 6
-#For two numbers
+# For two numbers
+
+
 def max_two_numbers(x, y):
-    
+
     if x > y:
         return x
-    
+
     return y
-print("This is the max number: ", max_two_numbers(5,7))
+
+print("This is the max number: ", max_two_numbers(5, 7))
+
 
 def max_three_numbers(x, y, z):
-    return max_two_numbers(x, max_two_numbers(y,z)) 
+    return max_two_numbers(x, max_two_numbers(y, z))
 
 print(max_three_numbers(3, 6, -10))
 
+# Write a function that can reverse string elements
+#exmple "455code" -> "edoc544"
+
+def reverse_string(string_val):
+    return string_val[:: -1]
+
+input_string = "UBRI"
+
+print(reverse_string(input_string))
 
