4f8695583b0c211c7787c76ab2fc6a2de9c80e62
Fred Denis <fredjamo68@gmail.com>
2023-03-25 11:24:58 +0300

    Home work

--- main.py
+++ main.py
@@ -43,11 +43,63 @@ class Employee:
     
 #Create object
 empl_1 = Employee()
+
 empl_1.name = "Ubri Hacker"
 print(empl_1.name)
 
 empl_1.years_of_work = 3
 print(f"Your salary is: ${empl_1.calculate_salary()}")
+
+#Homework Create a class Car, include all the attributes
+
+class Car:
+    #Define attributes of the car - colour, price, milage, 
+    # amount_of_fuel, number_plate, year
+    # function calculate the price of the car based on milage
+    # calculate the amount of fuel used per hour
+    brand = ""
+    color = ""
+    price = 1000000
+    milage = 200000
+    number_plate = "ru23x"
+    year = 2020
+    fuel_usage = 0
+    distance = 0
+    
+    def calculate_price(self):
+        if self.milage < 10000:
+            return self.price + 50000
+        return self.price
+    
+    def calculate_fuel_used(self):
+        fuel_used = self.distance / self.fuel_usage
+        return fuel_used
+    
+    # Write a function that prints out the color of the car
+    def car_color(self):
+        return self.color
+    
+    # Write a function that returns  the price of the car
+    
+
+# Create a new object
+
+car1 = Car()
+car1.price = 100000
+
+# Fuel used
+car1.distance = 1000
+car1.fuel_usage = 20
+car1.color = "Blue"
+
+print("The car color is:", car1.car_color())
+
+
+print("Price of fuel ", car1.calculate_fuel_used())
+print("Car price ",car1.calculate_price())
+        
+    
+    
     
         
     
