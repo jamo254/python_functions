46ea048b94485324e8ad779bea5b3df099a40387
Fred Denis <fredjamo68@gmail.com>
2023-03-25 11:52:17 +0300

    Constructors

---
+++ classes.py
@@ -0,0 +1,43 @@
+# Constructors
+
+class Plane:
+
+    def __init__(self):
+        self.color = "White"
+        self.price = 1000000
+
+        self.distance()
+
+    def distance(self):
+        print("Distance")
+
+
+plane1 = Plane()
+print(plane1.distance())
+
+
+class Person:
+  
+    def __init__(self, name, sex, profession, nationality):
+        # data members (instance variables)
+        self.name = name
+        self.sex = sex
+        self.profession = profession
+        self.nationality = nationality
+
+    # Behavior (instance methods)
+    def show(self):
+        print('Name:', self.name, 'Sex:', self.sex,
+              'Profession:', self.profession)
+
+    # Behavior (instance methods)
+    def work(self):
+        print(self.name, 'working as a', self.profession)
+    
+    def country(self):
+        return self.nationality
+    
+person = Person("Ubri", "Male", "Hacker", "Russian")
+print(person.name)
+print("Nationality: ", person.country())
+
